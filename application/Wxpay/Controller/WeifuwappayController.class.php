15cd38e130f3f7f71767cffbad3c5fa56563d049
luojianwei <kenvi.luo@gmail.com>
2017-12-18 18:58:14 +0800

    111

--- application/Wxpay/Controller/WeifuwappayController.class.php
+++ application/Wxpay/Controller/WeifuwappayController.class.php
@@ -144,6 +144,7 @@ class WeifuwappayController extends HomebaseController {
         	
         	$data = $this->wx_pay_db->where ( "from_order_sn='$from_order_sn'" )->find ();
         	
+        	
         	if ($data == null) {
         		$order_sn = sp_get_order_sn ();
         		
